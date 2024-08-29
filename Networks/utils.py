d79fea52c127d586cfbb9b699bed3af9a8d65803
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2024-08-30 02:44:49 +0300

    Update utils.py

--- Networks/utils.py
+++ Networks/utils.py
@@ -1,6 +1,7 @@
 from itertools import permutations
 from typing import Optional
 from random import choices
+import math
 import torch
 
 
@@ -39,3 +40,16 @@ def fix_activations(activations):
             if isinstance(activations[i], str):
                 activations[i] = {activations[i]: dict()}
     return activations
+
+
+def positional_encoding(x: torch.Tensor) -> torch.Tensor:
+    b, n, d = x.shape
+    position = torch.arange(0, n, dtype=x.dtype, device=x.device).unsqueeze(1)
+    div_term = torch.exp(torch.arange(0, d, 2, device=x.device, dtype=x.dtype) * -(math.log(10000.0) / d))
+
+    pos_enc = torch.zeros(1, n, d, device=x.device, dtype=x.dtype)
+
+    pos_enc[0, :, 0::2] = torch.sin(position * div_term)
+    pos_enc[0, :, 1::2] = torch.cos(position * div_term)
+
+    return x + pos_enc
