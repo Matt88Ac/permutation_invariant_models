d42f74d3907d6e4105d62edd0d7d8f5f92cc5c2b
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2024-08-30 01:33:33 +0300

    Update testing_funcs.py

--- symmetry_tests/testing_funcs.py
+++ symmetry_tests/testing_funcs.py
@@ -1,34 +1,39 @@
+from Networks.layers import *
+from symmetry_tests.testing_utils import test_invariance, test_equivariance, gen_random_data
 import torch
-
-from Networks.layers import GeneralLayer
-
-
-@torch.jit.script
-def MeanRE(y_pred: torch.Tensor, y_true: torch.Tensor):
-    """ Mean Relative Error """
-    error = (y_true - y_pred) / y_true
-    return error.abs().mean()
+import numpy as np
 
 
-def test_invariance(model: GeneralLayer, x: torch.Tensor, thr: float = 1e-4):
-    perm = torch.randperm(x.shape[1], device=x.device)
-    y1 = model(x[:, perm].clone())
-    y2 = model(x.clone())
-    return (MeanRE(y1, y2) < thr).cpu().detach().numpy().item()
+def gen_parameters(max_in_dim: int = 8, max_in_channels: int = 5, max_out_channels: int = 5,
+                   max_n_hidden: int = 7, max_scale: float = 20):
+    in_dim = np.random.randint(2, max_in_dim, size=1)[0]
+    in_channels = np.random.randint(2, max_in_channels, size=1)[0]
+    out_channels = np.random.randint(2, max_out_channels, size=1)[0]
+
+    n_hidden = np.random.randint(1, max_n_hidden, size=1)[0]
+    hidden = np.random.randint(1, max_in_channels, size=n_hidden)
+
+    scale = np.random.uniform(1, max_scale, 1)[0]
+
+    return in_dim, in_channels, out_channels, hidden, scale
 
 
-def test_equivariance(model: GeneralLayer, x: torch.Tensor, thr: float = 1e-4):
-    perm = torch.randperm(x.shape[1], device=x.device)
-    y1 = model(x[:, perm].clone())
-    y2 = model(x.clone())[:, perm]
-    return (MeanRE(y1, y2) < thr).cpu().detach().numpy().item()
+def test_model(model_type: MODEL_TYPES, batch_size: int, max_in_dim: int = 8, max_in_channels: int = 5,
+               max_out_channels: int = 5,
+               max_n_hidden: int = 7, max_scale: float = 20, thr: float = 1e-5,
+               device=torch.device('cpu'), dtype=torch.float64, **other_model_parameters):
+    in_dim, in_channels, out_channels, hidden, scale = gen_parameters(max_in_dim, max_in_channels, max_out_channels,
+                                                                      max_n_hidden, max_scale)
 
+    model = get_models(model_type)(in_dim, 1, in_channels, out_channels, hidden.tolist(),
+                                   device=device, dtype=dtype, **other_model_parameters)
+    x = gen_random_data(batch_size, (in_dim, in_channels), scale, device, dtype)
+    INV = test_invariance(model, x, thr)
 
-def gen_random_data(batch_size: int, shape: tuple, scale: float = 1, device=torch.device('cpu'),
-                    dtype=torch.float64) -> torch.Tensor:
-    x = torch.randn(batch_size, *shape, device=device, dtype=dtype) * scale
-    return x
+    model = get_models(model_type)(in_dim, 1, in_channels, out_channels, hidden.tolist(),
+                                   device=device, dtype=dtype, **other_model_parameters)
+    x = gen_random_data(batch_size, (in_dim, in_channels), scale, device, dtype)
 
-
-
+    EQ = test_equivariance(model, x, thr)
+    return INV or EQ
 
