8fdb1964db44b049bb5386dc22ae65085bc72c83
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2024-08-31 23:39:41 +0300

    Update run_classification.py

--- run_classification.py
+++ run_classification.py
@@ -1,12 +1,13 @@
 from time import time
-import torch
-from torch import nn
-from distribution_classification_experiment.data_generator import get_gaussian_dataloader, GaussianDataset
-from distribution_classification_experiment.classification_model import DistributionClassifier
-from tqdm import tqdm
 
 import numpy as np
 import pandas as pd
+import torch
+from torch import nn
+from tqdm import tqdm
+
+from distribution_classification_experiment.classification_model import DistributionClassifier
+from distribution_classification_experiment.data_generator import get_gaussian_dataloader
 
 
 @torch.no_grad()
@@ -19,7 +20,7 @@ def accuracy(y_pred, y_true) -> np.ndarray:
 def training_loop(model: DistributionClassifier, train_data, val_data,
                   n_samples: int, n_iter: int, lr: float = 1e-3, log_path: str = '',
                   **kwargs):
-    optimizer = torch.optim.Adamax(model.parameters(), lr=lr)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr, amsgrad=True)
 
     kwargs['device'] = model.device
     kwargs['dtype'] = model.dtype
@@ -36,11 +37,11 @@ def training_loop(model: DistributionClassifier, train_data, val_data,
             for x, y in train_data:
                 optimizer.zero_grad()
                 y_hat = model(x)
-                loss = criterion(y_hat[:, :1], y.to(dtype=y_hat.dtype))
+                loss = criterion(y_hat[:, 1:], y.to(dtype=y_hat.dtype))
                 loss.backward()
                 optimizer.step()
                 ovr_loss += loss.cpu().detach().numpy().item()
-                ovr_acc += accuracy(y_hat[:, :1], y)
+                ovr_acc += accuracy(y_hat[:, 1:], y)
                 i += 1
 
             train_loss = ovr_loss / i
@@ -53,9 +54,9 @@ def training_loop(model: DistributionClassifier, train_data, val_data,
                 i = 0
                 for x, y in val_data:
                     y_hat = model(x)
-                    loss = criterion(y_hat[:, :1], y.to(dtype=y_hat.dtype))
+                    loss = criterion(y_hat[:, 1:], y.to(dtype=y_hat.dtype))
                     ovr_loss += loss.cpu().detach().numpy().item()
-                    ovr_acc += accuracy(y_hat[:, :1], y)
+                    ovr_acc += accuracy(y_hat[:, 1:], y)
                     i += 1
                 val_loss = ovr_loss / i
             log[epoch, 2] = val_loss
@@ -76,17 +77,15 @@ def training_loop(model: DistributionClassifier, train_data, val_data,
 
 if __name__ == '__main__':
     # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    from sklearn.metrics import accuracy_score, RocCurveDisplay, roc_curve, roc_auc_score
     from matplotlib import pyplot as plt
 
     plt.style.use('ggplot')
     D = 5
     NS = [2, 4]
-    hidden = [16, 256, 16, 5]
-    LR = 1e-3
+    hidden = [8, 128, 128, 8, 5]
+    LR = 1e-4
     N_ITER = 5000
-    b_size = 256
-
+    BATCH_SIZE = 256
     activations = ['relu'] * (len(hidden))
     models = ['canonization_mlp', 'symmetrization_mlp', 'sample_symmetrization_mlp', 'canonization_transformer',
               'symmetrization_transformer', 'sample_symmetrization_transformer', 'equivariant', 'augmentation']
@@ -94,10 +93,8 @@ if __name__ == '__main__':
     for n_s in [100, 1000, 10000]:
 
         for n in NS:
-            train = get_gaussian_dataloader(b_size, n_s, (n, D), device='cuda:0', dtype=torch.float64, shuffle=True, num_workers=0)
-            val = get_gaussian_dataloader(b_size, min(n_s, 500), (n, D), device='cuda:0', dtype=torch.float64)
-
-            plt.figure(figsize=(20, 6), dpi=140)
+            train = get_gaussian_dataloader(BATCH_SIZE, n_s, (n, D), device='cuda:0', dtype=torch.float64, shuffle=True, num_workers=0)
+            val = get_gaussian_dataloader(BATCH_SIZE, n_s, (n, D), device='cuda:0', dtype=torch.float64)
 
             for kind in models:
                 class_model = DistributionClassifier(kind, (n, D), device='cuda', hidden_layers=hidden,
@@ -105,18 +102,3 @@ if __name__ == '__main__':
 
                 training_loop(class_model, train, val, n_s, n_iter=N_ITER,
                               lr=LR, log_path=f'classification_results/{kind}_{n}_')
-
-                test = GaussianDataset(10000, (n, D), device='cuda')
-                class_model.eval()
-                with torch.no_grad():
-                    pred = class_model(test.data)[:, :1].cpu().detach().numpy().squeeze()
-                    labels = test.labels.cpu().detach().numpy().squeeze()
-
-                fpr, tpr, _ = roc_curve(labels, pred)
-                score = roc_auc_score(labels, pred)
-                acc = accuracy_score(labels, (pred >= 0.5).astype(int))
-                print('\n', kind, acc, '\n')
-                roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr, estimator_name=f'{kind}: acc = {round(acc, 2)}',
-                                              roc_auc=score).plot(ax=plt.gca())
-            plt.legend(facecolor='gold', shadow=True, fancybox=True, edgecolor='k')
-            plt.savefig(f"classification_results/{n_s}_{n}.png", bbox_inches='tight')
